db215afb38427a74d718941fe5f5a5992fdb057b
EdgardoGuzman08 <edgardoguzman3108@gmail.com>
2023-05-16 22:10:51 -0600

    primero

---
+++ src/configuracion/Urls.js
@@ -0,0 +1,3 @@
+export const urlAPI = "http:192.168.100.2:2010/api";
+export const urlborrar = "http:192.168.56.1:2000/api/especialidades/eliminar?id=id"
+export const urlImagenesUsuarios = urlAPI + "/imagenes/clientes/";
